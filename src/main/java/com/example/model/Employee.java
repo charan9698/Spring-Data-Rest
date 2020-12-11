82ac959885688cbc44c9a9289454ab8a01006bda
RAMCHARANTEJA <RAMCHARANTEJA@Ramchran>
2020-12-12 00:50:01 +0530

    Model class Created

--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -1,5 +1,24 @@
 package com.example.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "emp_tab")
 public class Employee {
 
+	@Id
+	@GeneratedValue
+	private Integer empNo;
+	@Column(name = "emp_name_col",length = 10,nullable = false)
+	private String empName;
+	@Column(name = "emp_address_col",length = 25,nullable = false)
+	private String empAddress;
+	
 }
